77fcb73d4bd6d216f059a9677809db5e84bcd0dc
ziad <devnamedzed@gmail.com>
2019-03-28 22:48:14 -0700

    add missing ApiCallHandler to exports

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import { ApiFunction, ApiDelete, ApiGet, ApiPost, ApiPut } from './ApiFunction'
 import { ApiResponse, BinaryResponse, JsonResponse } from './ApiResponse'
+import ApiCallHandler from './ApiCallHandler'
 import ApiParameter from './ApiParameter'
 import ApiClient from './ApiClient'
 import HttpMethod from './HttpMethod'
 
 export { 
+    ApiCallHandler,
+    ApiClient, 
     ApiFunction, 
     ApiDelete,
     ApiGet, 
     ApiPost, 
     ApiPut, 
     ApiParameter, 
-    ApiClient, 
     HttpMethod,
     ApiResponse, 
     BinaryResponse, 
